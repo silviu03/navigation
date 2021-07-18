b3fe1f8d60b93d914c965067e5ba3013e04ba658
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-07-18 16:50:05 +0300

    new

--- navigation.js
+++ navigation.js
@@ -1,48 +1,82 @@
 document.addEventListener("DOMContentLoaded", function(ev) {
     const navItems = document.getElementsByClassName("menu-item")
     const navPages = document.getElementsByClassName("target-item")
-    // const 
+    const logoIcons = document.getElementsByClassName("logo-icon")
+    const orderDetails = document.getElementById("order-details-page")
+    const orderBoxDetails = document.querySelector(".order-details-box")
+    const orderBox = document.querySelector(".orders-box")
+    
+
     console.log(navItems);
     console.log(navPages);
+    console.log(logoIcons);
+    console.log(orderDetails);
+    console.log(orderBox);
 
     for(let i = 0; i < navItems.length; i++) {
         navItems[i].addEventListener("click", function(event) {
+            orderBoxDetails.classList.add("hidden")
             if (navItems[i].classList.contains("selected")) {
-                console.log("menu works")
+                
                 navItems[i].classList.remove("selected")
-                console.log("it works 2")
+                
             }else{
                 navItems[i].classList.add("selected")
-                console.log("it works 3")
+                
             }
-            console.log("it works 4")
+            
 
             if(navPages[i].classList.contains("hidden")) {
-                console.log("page works")
+                
                 navPages[i].classList.remove("hidden")
-                console.log("page works 2")
+                
             }
 
+            if(logoIcons[i].classList.contains("hidden")) {
+                logoIcons[i].classList.remove("hidden")
+            }
+
+            
             
             for (let j = 0; j < navItems.length; j++) {
                 if(i != j) {
                     console.log(navItems[i])
                     navItems[j].classList.remove("selected")
-                    console.log("it works 5")
+                    
                     if(!navPages[j].classList.contains("hidden")) {
                         navPages[j].classList.add("hidden")
                     }
+
+                    if(!logoIcons[j].classList.contains("hidden")) {
+                        logoIcons[j].classList.add("hidden")
+                    }
+
                 }
                 
 
             }
         })
     }
+
+
+
     
+        orderDetails.addEventListener("click", function(orderEvent) {
+            if(!orderBox.classList.contains("hidden")) {
+                orderBox.classList.add("hidden")
+            }
+            if(orderBoxDetails.classList.contains("hidden")) {
+                orderBoxDetails.classList.remove("hidden")
+            }
+        });
 
 
 
 
 
 
-});
+
+});  
+
+
+    
