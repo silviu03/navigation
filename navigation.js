2d06e74df09c7956544d0f7f8a925f30c8361c07
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-07-27 21:42:46 +0300

    update with submit

--- navigation.js
+++ navigation.js
@@ -1,82 +1,86 @@
-document.addEventListener("DOMContentLoaded", function(ev) {
+document.addEventListener("DOMContentLoaded", function (ev) {
     const navItems = document.getElementsByClassName("menu-item")
     const navPages = document.getElementsByClassName("target-item")
     const logoIcons = document.getElementsByClassName("logo-icon")
     const orderDetails = document.getElementById("order-details-page")
     const orderBoxDetails = document.querySelector(".order-details-box")
     const orderBox = document.querySelector(".orders-box")
-    
-
-    console.log(navItems);
-    console.log(navPages);
-    console.log(logoIcons);
-    console.log(orderDetails);
-    console.log(orderBox);
-
-    for(let i = 0; i < navItems.length; i++) {
-        navItems[i].addEventListener("click", function(event) {
+ 
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener("click", function (event) {
             orderBoxDetails.classList.add("hidden")
             if (navItems[i].classList.contains("selected")) {
-                
                 navItems[i].classList.remove("selected")
-                
-            }else{
+            } else {
                 navItems[i].classList.add("selected")
-                
             }
-            
 
-            if(navPages[i].classList.contains("hidden")) {
-                
+            if (navPages[i].classList.contains("hidden")) {
                 navPages[i].classList.remove("hidden")
-                
             }
 
-            if(logoIcons[i].classList.contains("hidden")) {
+            if (logoIcons[i].classList.contains("hidden")) {
                 logoIcons[i].classList.remove("hidden")
             }
 
-            
-            
             for (let j = 0; j < navItems.length; j++) {
-                if(i != j) {
-                    console.log(navItems[i])
+                if (i != j) {
                     navItems[j].classList.remove("selected")
-                    
-                    if(!navPages[j].classList.contains("hidden")) {
+
+                    if (!navPages[j].classList.contains("hidden")) {
                         navPages[j].classList.add("hidden")
                     }
 
-                    if(!logoIcons[j].classList.contains("hidden")) {
+                    if (!logoIcons[j].classList.contains("hidden")) {
                         logoIcons[j].classList.add("hidden")
                     }
-
                 }
-                
-
             }
         })
     }
 
+    orderDetails.addEventListener("click", function (orderEvent) {
+        if (!orderBox.classList.contains("hidden")) {
+            orderBox.classList.add("hidden")
+        }
+        if (orderBoxDetails.classList.contains("hidden")) {
+            orderBoxDetails.classList.remove("hidden")
+        }
+    });
+
+    const loaderOverlay = document.getElementById('loaderOverlay');
+    const modalOverlay = document.getElementById('modalOverlay');
+    const accountInfoData = document.getElementById('accountInfoData');
+    console.log(accountInfoData);
+
+    accountInfoData.addEventListener('submit', function(eventAccountInfo) {
+        console.log('wtf!!')
+        eventAccountInfo.preventDefault();
+        loaderOverlay.classList.remove('hidden');
+        console.log('wtf')
+
+        setTimeout(function() {
+            loaderOverlay.classList.add('hidden');
+            modalOverlay.classList.remove('hidden');
+
+            setTimeout(function() {
+                modalOverlay.classList.add('hidden');
+            }, 4000);
+        }, 4000);
+
+    })
+
+    const closeBtn = document.getElementsByClassName('close-btn');
+    closeBtn[0].addEventListener('click', function(eventCloseBtn) {
+        loaderOverlay.classList.add('hidden');
+        modalOverlay.classList.add('hidden');
+        console.log('is it closing ???')
+    })
 
 
-    
-        orderDetails.addEventListener("click", function(orderEvent) {
-            if(!orderBox.classList.contains("hidden")) {
-                orderBox.classList.add("hidden")
-            }
-            if(orderBoxDetails.classList.contains("hidden")) {
-                orderBoxDetails.classList.remove("hidden")
-            }
-        });
+
+
+});
 
 
 
-
-
-
-
-});  
-
-
-    
