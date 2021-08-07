11591add401df1771ab3c3ec1c1ab4537fcbc076
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-08-07 20:42:11 +0300

    new update jquery

--- navigation.js
+++ navigation.js
@@ -1,87 +1,78 @@
-document.addEventListener("DOMContentLoaded", function (ev) {
-    const navItems = document.getElementsByClassName("menu-item")
-    const navPages = document.getElementsByClassName("target-item")
-    const logoIcons = document.getElementsByClassName("logo-icon")
-    const orderDetails = document.getElementById("order-details-page")
-    const orderBoxDetails = document.querySelector(".order-details-box")
-    const orderBox = document.querySelector(".orders-box")
- 
-    for (let i = 0; i < navItems.length; i++) {
-        navItems[i].addEventListener("click", function (event) {
-            orderBoxDetails.classList.add("hidden")
-            if (navItems[i].classList.contains("selected")) {
-                navItems[i].classList.remove("selected")
-            } else {
-                navItems[i].classList.add("selected")
+$(function () {
+    console.log('ready');
+
+    const meniuItems = $('.menu-item');
+    const navPages = $('.target-item');
+
+    $('#order-details-page').click(function () {
+        $('.order-details-box').removeClass('hidden');
+        navPages.addClass('hidden');
+    })
+
+    $('.item1').click(function () {
+        $('.orders-box').removeClass('hidden');
+        $('.order-details-box').addClass('hidden');
+       
+    })
+
+
+    meniuItems.click(function (e) {
+        let data = $(this).data('link-order');
+
+        navPages.each(function (index, item) {
+            
+            if (!$(item).hasClass('hidden')) {
+                $(item).addClass('hidden')
             }
 
-            if (navPages[i].classList.contains("hidden")) {
-                navPages[i].classList.remove("hidden")
+            if (data == index) {
+                $(item).removeClass('hidden')
             }
-
-            if (logoIcons[i].classList.contains("hidden")) {
-                logoIcons[i].classList.remove("hidden")
-            }
-
-            for (let j = 0; j < navItems.length; j++) {
-                if (i != j) {
-                    navItems[j].classList.remove("selected")
-
-                    if (!navPages[j].classList.contains("hidden")) {
-                        navPages[j].classList.add("hidden")
-                    }
-
-                    if (!logoIcons[j].classList.contains("hidden")) {
-                        logoIcons[j].classList.add("hidden")
-                    }
-                }
-            }
+            
         })
-    }
+    })
 
-    orderDetails.addEventListener("click", function (orderEvent) {
-        if (!orderBox.classList.contains("hidden")) {
-            orderBox.classList.add("hidden")
-        }
-        if (orderBoxDetails.classList.contains("hidden")) {
-            orderBoxDetails.classList.remove("hidden")
-        }
-    });
-
-    const loaderOverlay = document.getElementById('loaderOverlay');
-    const modalOverlay = document.getElementById('modalOverlay');
-    const inputSubmit = document.getElementById('inputSubmit');
-    const inputForm = document.getElementById('input-form');
-    console.log(inputForm);
-
-    inputForm.addEventListener('submit', function(eventAccountInfo) {
-    
-        eventAccountInfo.preventDefault();
-        loaderOverlay.classList.remove('hidden');
-        console.log('wtf')
-
+    $('#inputSubmit').click(function(event) {
+        event.preventDefault();
+        
+        $('#loaderOverlay').removeClass('hidden')
         setTimeout(function() {
-            loaderOverlay.classList.add('hidden');
-            modalOverlay.classList.remove('hidden');
-
-            // setTimeout(function() {
-            //     modalOverlay.classList.add('hidden');
-            // }, 4000);
+            $('#loaderOverlay').addClass('hidden')
+            $('#modalOverlay').removeClass('hidden')
         }, 4000);
-
     })
 
-    const closeBtn = document.getElementsByClassName('close-btn');
-    closeBtn[0].addEventListener('click', function(eventCloseBtn) {
-        loaderOverlay.classList.add('hidden');
-        modalOverlay.classList.add('hidden');
-        console.log('is it closing ???')
+    $('.close-btn').click(function() {
+        $('#modalOverlay').addClass('hidden')
     })
 
 
 
 
-});
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+1
